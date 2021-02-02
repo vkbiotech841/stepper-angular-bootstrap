fe8509d6262df32c49d91096413133320ae69dcf
VIKRAM KUMAR <vkbiotech841@gmail.com>
2021-02-02 16:18:10 +0530

    finished stpeer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stepper-angular';
+
+
+  tabItems = [
+    {
+      name: 'Personals Details',
+      isActive: true
+    },
+    {
+      name: 'Address Details',
+      isActive: false
+    },
+    {
+      name: 'Education Details',
+      isActive: false
+    },
+    {
+      name: 'Professional Details',
+      isActive: false
+    },
+  ];
+
+
+  selectedTabIndex: number = 0;
+
+  toggleActiveClass(index: any) {
+    switch (index) {
+      case 0:
+        this.selectedTabIndex = index;
+        console.log("selectedTabIndex", index);
+        break;
+      case 1:
+        this.selectedTabIndex = index;
+        console.log("selectedTabIndex", index);
+        break;
+      case 2:
+        this.selectedTabIndex = index;
+        console.log("selectedTabIndex", index);
+        break;
+      case 3:
+        this.selectedTabIndex = index;
+        console.log("selectedTabIndex", index);
+        break;
+
+      default:
+        this.selectedTabIndex = 0;
+        break;
+    }
+  }
+
+  moveToNextSlide(slideIndex: any) {
+    this.selectedTabIndex = slideIndex;
+  }
 }
